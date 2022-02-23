b7ae4f75159969a3137a0a57bf889dacbeb83bff
HyuckJuneHong <jjj8337721@naver.com>
2022-02-23 10:33:05 +0900

    Hackers

---
+++ src/hackerrank/ictCodingTest/Ex4.java
@@ -0,0 +1,23 @@
+package hackerrank.ictCodingTest;
+
+import java.util.List;
+
+public class Ex4 {
+
+
+    /*
+     * Complete the 'findSchedules' function below.
+     *
+     * The function is expected to return a STRING_ARRAY.
+     * The function accepts following parameters:
+     *  1. INTEGER workHours
+     *  2. INTEGER dayHours
+     *  3. STRING pattern
+     */
+
+    public static List<String> findSchedules(int workHours, int dayHours, String pattern) {
+        // Write your code here
+        return null;
+    }
+
+}
